d512c27fa19398e8f995c19382fb069bd1c98000
DmitrijLosev <dmitrijslosevs@inbox.lv>
2023-11-14 22:58:23 +0200

    8kyu

--- main_8kyu.js
+++ main_8kyu.js
@@ -65,13 +65,62 @@ function lovefunc(flower1, flower2) {
 }
 console.log(lovefunc(2, 2))
 
+//DNA to RNA Conversion
+//https://www.codewars.com/kata/5556282156230d0e5e000089
+function DNAtoRNA(dna) {
+    return dna.split("").map(i=>(i==="T" ? i="U" : i)).join("")
+}
+console.log(DNAtoRNA("TTTT"))
 
+//If you can't sleep, just count sheep!!
+//https://www.codewars.com/kata/5b077ebdaf15be5c7f000077/train/javascript
 
+function countSheep(num){
+    let arr=[]
+    for (let i = 0; i < num; i++) {
+        arr[i]=`${i+1} sheep...`
+    }
+    return arr.join("")
+}
 
+console.log(countSheep(3))
 
+//Jenny's secret message
+//https://www.codewars.com/kata/55225023e1be1ec8bc000390
 
+function greet(name){
+    if(name === "Johnny")
+    {return "Hello, my love!"} else
+    {return "Hello, " + name + "!";}
+}
 
+//Count by X
+//https://www.codewars.com/kata/5513795bd3fafb56c200049e
 
+function countBy(x, n) {
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+        arr[i]=x*(i+1)
+    }
+    return arr;
+}
 
+console.log(countBy(2, 5))
 
+//Find Maximum and Minimum Values of a List
+//https://www.codewars.com/kata/577a98a6ae28071780000989
 
+function min(list){
+    for (let i = 0; i < list.length; i++) {
+        list[i]<list[i+1] ? list[i+1]=list[i] : ""
+    }
+    return list[list.length-1];
+}
+console.log(min([1, 2, 3]))
+function max(list){
+    for (let i = 0; i < list.length; i++) {
+        list[i]>list[i+1] ? list[i+1]=list[i] : ""
+    }
+    return list[list.length-1];
+}
+console.log(max([7, 2, 3]))
